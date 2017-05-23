ec52a8236454e0ce8d42bb77e838fdbe9928acd5
suri-h-takahashi <h_takahashi@suri.co.jp>
2017-05-23 18:29:52 +0900

    BBBBBBに変更

--- ztest/src/coverage/Foo.java
+++ ztest/src/coverage/Foo.java
@@ -2,7 +2,7 @@ package coverage;
 
 public class Foo {
 	public String methodB(String stringValue) {
-		return " AAAAAAAAAAFoo.methodB call with " + stringValue;
+		return " BBBBBBBBFoo.methodB call with " + stringValue;
 	}
 
 	public String methodC(int intValue) {
