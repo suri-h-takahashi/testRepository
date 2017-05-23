7b2dfe0e0079151c7e7b6d0ac4f786e616b70e25
suri-h-takahashi <h_takahashi@suri.co.jp>
2017-05-23 16:25:45 +0900

    test2

--- ztest/src/coverage/Foo.java
+++ ztest/src/coverage/Foo.java
@@ -2,7 +2,7 @@ package coverage;
 
 public class Foo {
 	public String methodB(String stringValue) {
-		return " Foo.methodB call with " + stringValue;
+		return " AAAAAAAAFoo.methodB call with " + stringValue;
 	}
 
 	public String methodC(int intValue) {
